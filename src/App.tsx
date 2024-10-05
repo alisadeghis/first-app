dfbffdb3997761e516a71ace33093eb847673b12
a.sadeghi <thealisadeghi81@gmail.com>
2024-10-05 18:45:28 +0330

    add basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import './App.css'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-function App() {
-
-  return (
-    <></>
-  )
-}
-
-export default App
+const App = () => {
+  return <></>;
+};
+export default App;
